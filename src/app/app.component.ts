052f468b1ed52ec559f92a584b8923c9242dd5e8
Pedro Carvi <pedro.carvi-11@outlook.es>
2024-02-02 14:50:57 -0300

    1 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'inline-table-editing';
+  personas = [
+    { nombre: 'Juan', apellido: 'Pérez', edad: 25 },
+    { nombre: 'María', apellido: 'González', edad: 30 },
+    { nombre: 'Carlos', apellido: 'López', edad: 22 },
+    { nombre: 'Laura', apellido: 'Martínez', edad: 28 },
+    { nombre: 'Pedro', apellido: 'Sánchez', edad: 35 },
+  ];
+  mostrarInputs = false;
+  nuevoNombre: string = '';
+  nuevoApellido: string = '';
+  nuevaEdad: number = 0;
+
+  guardarNuevo() {
+    this.personas.push({
+      nombre: this.nuevoNombre,
+      apellido: this.nuevoApellido,
+      edad: this.nuevaEdad,
+    });
+    this.resetForm();
+  }
+
+  cancelarNuevo() {
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.mostrarInputs = false;
+    this.nuevoNombre = '';
+    this.nuevoApellido = '';
+    this.nuevaEdad = 0;
+  }
 }
